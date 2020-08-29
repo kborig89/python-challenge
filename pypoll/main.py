fcd0ee4853cc813a28e37980bd5cdd2a7a8bde1f
kathrynbuckwalter <kborig89@gmail.com>
2020-08-28 21:32:59 -0400

    pypoll updated

--- pypoll/main.py
+++ pypoll/main.py
@@ -1,27 +1,64 @@
 import os
 import csv
 
-csvpath=os.path.join('..','homework','python_homework''election_data.csv')
-voter_id[]
-county[]
-candidate[]
+csvpath=os.path.join('resources','election_data.csv')
 
-print("Election Results")
-print("------------------------")
+voter_id=[]
+county=[]
+candidate=[]
+khan_count=0
+li_count=0
+correy_count=0
+otooley_count=0
+vote_count=0
 
-with open(udemy_csv,encoding='utf-8') as csvfile:
+
+with open(csvpath,encoding='utf-8') as csvfile:
     csvreader=csv.reader(csvfile,delimiter=',')
+    csv_header = next(csvreader)
     for row in csvreader:
-    csv_header=next(csvreader)
         voter_id.append(row[0])
         county.append(row[1])
         candidate.append(row[2])
 
+        if row[2]=="Khan":
+            khan_count +=1
+        elif row[2] == "Li":
+            li_count +=1
+        elif row[2] == "Correy":
+            correy_count +=1
+        else:
+            otooley_count +=1
 
-print("Total Votes =..."+str(len(voter_id)))
+
+print("Election Results")
+print("------------------------")
+print("Total Votes =" + str(len(voter_id)))
+print('---------------------------')
 
 
-exit()
-output_file = os.path.join("budget_data_complete")
- with open(file_to_output, "w") as txt_file:
-    txt_file.write(output)
+candidate=["Khan","Li","Correy","Otooley"]
+vote_count=[khan_count,li_count,correy_count,otooley_count]
+
+candidate_vote_count=zip(candidate,vote_count)
+
+khan_percent=round(khan_count/(len(voter_id)),3)*100
+li_percent=round(li_count/(len(voter_id)),3)*100
+correy_percent=round(correy_count/(len(voter_id)),3)*100
+otooley_percent=round(otooley_count/(len(voter_id)),3)*100
+winner=(max(vote_count))
+
+print(f'Khan:{khan_percent}% ({khan_count})')
+print(f'li:{li_percent}% ({li_count})')
+print(f'correy:{correy_percent}% ({correy_count})')
+print(f'otooley:{otooley_percent}% ({otooley_count})')
+print('---------------------------')
+print(f'Winner: {winner}')
+print('---------------------------')
+
+
+
+
+
+
+
